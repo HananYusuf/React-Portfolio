1b8f46b6e802f8f1072936576b4acca241aebc42
Hanan Yusuf <afetyusuf.gggnc@gmail.com>
2021-01-19 23:45:22 -0600

    readme file updated

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # React-Portfolio
 
+![License](https://img.shields.io/badge/LICENSE-MIT-GREEN)
+
+## Description
+
+This is a responsive personal portfolio built with HTML, CSS, Bootstrap and React.js. The deployed application can be found [Here](https://hananyusuf.github.io/React-Portfolio/)
+
+
+## Questions?
+
+For any questions you may have, feel free to find me via the information provided below:
+
+Email:
+afetyusuf.gggnc@gmail.com
+
+[Github Page](https://github.com/HananYusuf/)
+HananYusuf
--- src/pages/About.js
+++ src/pages/About.js
@@ -15,7 +15,7 @@ function About() {
                         <article className="col-lg-10">
                             <img src={pic} hspace='5' className="img-fluid float-md-left rounded" alt="Hanan Yusuf" />
                             <p>My name is Hanan Yusuf and I am a Full-Stack Web Development student at Rice University Coding Boot Camp, currently residing in Houston, Tx.</p>
-                            <p>As a beginner developer, I have aquired a basic working knowledge of HTML, CSS, JavaScript and Jquery in my four weeks of the boot camp.</p>
+                            <p>As a beginner developer, I have aquired a working knowledge of HTML, CSS, JavaScript, Jquery, MySQL, Mongoose, NodeJS and ReactJs in my three months of the boot camp.</p>
                             <p>Learning and exprementing new technologies motivates me to work hard, move forward to pursue and accomplish my dream of becoming a developer. </p>
 
                         </article>
